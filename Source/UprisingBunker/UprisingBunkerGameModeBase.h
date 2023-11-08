3fca35f5dd383a2a8cb9c620f5de61515bee9c59
Stephen1902 <s_nichols_other@yahoo.co.uk>
2023-11-08 15:18:33 +0000

    Initial Commit

---
+++ Source/UprisingBunker/UprisingBunkerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UprisingBunkerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UPRISINGBUNKER_API AUprisingBunkerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
